5ba3b1ede9076799ff582fc6f9f130bd475fd5de
Rashad Dixon <dixoncodes@gmail.com>
2019-12-12 21:35:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,6 +11,9 @@ def introduction_with_language(name, language){
   puts "Hi, my nmae is #{name} and I am learning to program in #{language}"
 }
 
+def introduction_with_language_optional(name, language = "Ruby"){
+  puts "Hi, my nmae is #{name} and I am learning to program in #{language}"
+}
 
 
 
